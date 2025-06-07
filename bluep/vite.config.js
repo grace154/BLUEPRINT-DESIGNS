9ab32518d2b4078dc262c886d594214467240930
unknown <Grace Nyambura>
2025-06-07 17:36:25 +0300

    Update

--- bluep/vite.config.js
+++ bluep/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
- base: './', // 👈 Add this line  
+ base: '/', // 👈 Add this line  
   plugins: [react()],
 })
